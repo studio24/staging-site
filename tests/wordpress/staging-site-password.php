35ed61bafc3e9a3ee9f0cb1de497c395efefd783
Simon R Jones <simon@studio24.net>
2026-04-11 19:22:20 +0100

    Removing test files

--- tests/wordpress/staging-site-password.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('WP_ENVIRONMENT_TYPE', 'staging');
-define('STAGING_SITE_PASSWORD', '$2y$10$pvDrmW9GW0bLVDMtLwLnn.mFof5eQQwlozZ5kYK7xijQRVAWUw0FK');
-
-\Studio24\StagingSite\StagingSite::run('WP_ENVIRONMENT_TYPE', 'staging');
