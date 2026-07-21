682d1e5f19aecd30733eae8b52ef8055fd389439
Gareth Trinkwon <gareth@studio24.net>
2026-07-21 12:43:27 +0100

    fix: Allow WP CLI commands to run correctly by skipping StagingSite plugin when WP_CLI constant is true.

--- wordpress/staging-site.php
+++ wordpress/staging-site.php
@@ -1,3 +1,5 @@
 <?php
 
-\Studio24\StagingSite\StagingSite::run('WP_ENVIRONMENT_TYPE', 'staging');
+if (!defined( 'WP_CLI' ) || !WP_CLI) {
+    \Studio24\StagingSite\StagingSite::run('WP_ENVIRONMENT_TYPE', 'staging');
+}
